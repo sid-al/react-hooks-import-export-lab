e5c05ffb1af9ab3c5c39122df7a68525bc7bd999
sid-al <sendplusx@gmail.com>
2022-07-18 16:17:15 +0300

    Done.

--- src/data/user.js
+++ src/data/user.js
@@ -4,5 +4,5 @@ const city = "New York";
 const image = "https://i.imgur.com/mV8PQxj.gif";
 
 // export all three variables as named exports
-export { username,city }
+export { username,city}
 export default image;
